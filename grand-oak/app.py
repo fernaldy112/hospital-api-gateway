60250e578963b1f9be051fa36676e58768b8cd56
Radenz <radenrifqirahman@gmail.com>
2023-11-14 21:02:10 +0700

    feat: services

--- grand-oak/app.py
+++ grand-oak/app.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+from random import choice
 app = Flask(__name__)
+
+doctors = [
+    'Rhys Hawkins',
+    'Vilma Jarvi',
+    'Ted Ellison',
+    'Heath Atwood',
+    'Kinslee Fink'
+]
+
+hours = list(range(8, 16))
+minutes = [0, 15, 30, 45]
+
 
 @app.route("/help", methods=["GET"])
 def getHelp():
-	return {"name": "grand-oak"}
+    return {"name": "grand-oak"}
+
+
+@app.route("/appointment", methods=["POST"])
+def createAppointment():
+    doctor = choice(doctors)
+    hour = choice(hours)
+    minute = choice(minutes)
+    time = f"{hour:02}:{minute:02}"
+    data = {
+        "hospital": "Grand Oak Hospital",
+        "doctor": doctor,
+        "time": time,
+    }
+    return data
--- pine-valley/app.py
+++ pine-valley/app.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+from random import choice
 app = Flask(__name__)
+
+doctors = [
+    'Joshua Wilson',
+    'Victoria Roach',
+    'Ellis Schaefer',
+    'Regan Rosen',
+    'Daisy Morgan'
+]
+
+hours = list(range(8, 16))
+minutes = [0, 15, 30, 45]
+
 
 @app.route("/help", methods=["GET"])
 def getHelp():
-	return {"name": "pine-valley"}
+    return {"name": "pine-valley"}
+
+
+@app.route("/appointment", methods=["POST"])
+def createAppointment():
+    doctor = choice(doctors)
+    hour = choice(hours)
+    minute = choice(minutes)
+    time = f"{hour:02}:{minute:02}"
+    data = {
+        "hospital": "Pine Valley",
+        "doctor": doctor,
+        "time": time,
+    }
+    return data
