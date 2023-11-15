eaa64c96ee0ebf2d913d8e40228c63a30784fde2
Radenz <radenrifqirahman@gmail.com>
2023-11-15 22:46:41 +0700

    feat: clean setup script, add appointment template script

---
+++ appointment.sh
@@ -0,0 +1,1 @@
+curl -X POST http://localhost:8000/appointment --data name="INSERT NAME HERE" --data date="INSERT DATE HERE"
--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
-# Create services
-curl -i -s -X POST http://localhost:8001/services --data name=pine-valley --data url='http://pine-valley:5000'
-curl -i -s -X POST http://localhost:8001/services --data name=grand-oak --data url='http://grand-oak:5000'
-
-# Create routes
-curl -i -X POST http://localhost:8001/services/pine-valley/routes --data 'paths[]=/reserve' --data name=pine-valley-route
-curl -i -X POST http://localhost:8001/services/grand-oak/routes --data 'paths[]=/reserve' --data name=grand-oak-route
-
 # Create upstream service
 curl -X POST http://localhost:8001/upstreams --data name=upstream
 
@@ -13,6 +5,8 @@ curl -X POST http://localhost:8001/upstreams --data name=upstream
 curl -X POST http://localhost:8001/upstreams/upstream/targets --data target='pine-valley:5000'
 curl -X POST http://localhost:8001/upstreams/upstream/targets --data target='grand-oak:5000'
 
-# Update services' host
-curl -X PATCH http://localhost:8001/services/pine-valley --data host='upstream'
-curl -X PATCH http://localhost:8001/services/grand-oak --data host='upstream'
+# Create services
+curl -i -s -X POST http://localhost:8001/services --data name=hospital --data host='upstream'
+
+# Create routes
+curl -i -X POST http://localhost:8001/services/hospital/routes --data 'paths[]=/' --data name=hospital-route
